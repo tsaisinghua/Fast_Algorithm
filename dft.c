72310c622e82ba4f760c4387192c20486e5e9e2b
tsaisinghua <36875739+tsaisinghua@users.noreply.github.com>
2018-05-01 09:24:57 +0800

    add dft.c file

---
+++ dft.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <omp.h>
+#define DEBUG 0
+int main()
+{
+	int n, k, N = 1024;
+	double c, s, theta, *X_re, *X_im, *x_re, *x_im;
+	clock_t t1, t2;
+	
+	X_re = (double *) malloc(N*sizeof(double));
+	X_im = (double *) malloc(N*sizeof(double));	
+	x_re = (double *) malloc(N*sizeof(double));
+	x_im = (double *) malloc(N*sizeof(double));
+	
+	for(n=0;n<N;++n)
+	{
+		x_re[n] = n+1;
+		x_im[n] = 0.0;
+	}
+	
+	t1 = clock();
+	for(k=0;k<N;++k)
+	{
+		X_re[k] = 0.0;
+		X_im[k] = 0.0;
+		// x_n = x_re + i* x_im
+		// x_n *(cos(...)- i * sin(...))
+		//real:x_re * cos(...) +  x_im * sin(...)
+		//image:x_im * cos(...) -  x_re * sin(...)
+		for(n=0;n<N;++n)
+		{
+			theta = 2*M_PI*k*n/N;
+			c = cos(theta);
+			s = sin(theta);
+			X_re[k] += x_re[n] * c +  x_im[n] * s;
+			X_im[k] += x_im[n] * c -  x_re[n] * s;
+		}
+	}
+	t2 = clock();	
+	
+	printf("Time: %f\n",1.0*(t2-t1)/CLOCKS_PER_SEC);
+	
+	#if DEBUG
+	for(k=0; k<N; ++k)
+	{
+		if(X_im[k] >= 0)
+			printf("%f + %f i\n", X_re[k], X_im[k]);
+		else
+			printf("%f - %f i\n", X_re[k], -X_im[k]);
+	}
+	#endif
+	return 0;
+} 
