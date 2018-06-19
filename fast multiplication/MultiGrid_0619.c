1a5fa15b20a43db630bdcb0c8438b286b2138b87
tsaisinghua <36875739+tsaisinghua@users.noreply.github.com>
2018-06-19 15:05:52 +0800

    0619

---
+++ fast multiplication/MultiGrid_0619.c
@@ -0,0 +1,252 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+int Jacobi_Iteration(double **U, double **F, int N);
+int GaussSeidel_Iteration(double **U, double **F, int N);
+int Multigrid_Iteration(double **U, double **F, int N);
+double Residual(double **R, double **U, double **F, int N);
+int main()
+{
+	int i, j, k, p, N, Max_Steps = 100, M;
+	double h, **U, **F, **R, r, r0, r1;
+	time_t t1, t2;
+	
+	p = 8;
+	N = 1 << p;
+	M = (N-1)*(N-1); 		// n=4 只有 (4-1) 個未知數 
+	printf("N = %d\n",N);
+	printf("Total Number of Unknowns = %d\n", M);
+	h = M_PI/N;
+	U = (double **) malloc( (N-1)*sizeof(double*) );
+	U[0] = (double *) malloc( (N-1)*(N-1)*sizeof(double) );
+	for(i=1;i<N-1;++i) U[i] = U[i-1] + (N-1);
+	F = (double **) malloc( (N-1)*sizeof(double*) );
+	F[0] = (double *) malloc( (N-1)*(N-1)*sizeof(double) );
+	for(i=1;i<N-1;++i) F[i] = F[i-1] + (N-1);
+	R = (double **) malloc( (N-1)*sizeof(double*) );
+	R[0] = (double *) malloc( (N-1)*(N-1)*sizeof(double) );
+	for(i=1;i<N-1;++i) R[i] = R[i-1] + (N-1);
+	
+	for(i=0;i<N-1;++i)
+	{
+		for(j=0;j<N-1;++j)
+		{
+			U[i][j] = 0.0;
+			F[i][j] = 1.0*h*h;
+		}
+	}
+	r = r0 = Residual(R, U, F, N);
+	k = 0;
+	t1 = clock();
+	while(r > r0*1e-6 && k < Max_Steps)
+	{
+		Multigrid_Iteration(U, F, N);		// 每做一次就，
+		r1 = Residual(R, U, F, N);			// 計算殘差量 
+		k ++;
+		printf("%d:%e %f\n",k,r1,r1/r);
+		r = r1;
+	}
+	t2 = clock();
+	printf("Total time: %f\n", 1.0*(t2-t1)/CLOCKS_PER_SEC);
+	return 0;
+}
+int Jacobi_Iteration(double **U, double **F, int N)
+{
+	int i, j;
+	double s, **V;
+
+	V = (double **) malloc( (N-1)*sizeof(double*) );
+	V[0] = (double *) malloc( (N-1)*(N-1)*sizeof(double) );
+	for(i=1;i<N-1;++i) V[i] = V[i-1] + (N-1);
+	
+	for(i=0;i<N-1;++i) 
+	{
+		for(j=0;j<N-1;++j)
+		{
+			s = F[i][j];
+			if(j-1>=0) s -= U[i][j-1];
+			if(j+1< N-1) s -= U[i][j+1];
+			if(i-1>=0) s -= U[i-1][j];
+			if(i+1< N-1) s -= U[i+1][j];
+			V[i][j] = s/(-4.0);
+		}
+	}
+	for(i=0;i<N-1;++i) 
+	{
+		for(j=0;j<N-1;++j)
+		{
+			U[i][j] = V[i][j];
+		}
+	}
+	free(V[0]);
+	free(V);
+	
+	return 0;
+}
+int GaussSeidel_Iteration(double **U, double **F, int N)
+{
+	int i, j;
+	double s;
+	
+	for(i=0;i<N-1;++i) 
+	{
+		for(j=0;j<N-1;++j)
+		{
+			s = F[i][j];
+			if(j-1>=0) s -= U[i][j-1];
+			if(j+1< N-1) s -= U[i][j+1];
+			if(i-1>=0) s -= U[i-1][j];
+			if(i+1< N-1) s -= U[i+1][j];
+			U[i][j] = s/(-4.0);
+		}
+	}
+	
+	/*   //逆向跑，多跑幾次，先 MARK 起來才看得出 GaussSeidel_Iteration 在做啥。 
+	for(i=0;i<N-1;++i) 
+	{
+		for(j=N-2;j>=0;j--)
+		{
+			s = F[i][j];
+			if(j-1>=0) s -= U[i][j-1];
+			if(j+1< N-1) s -= U[i][j+1];
+			if(i-1>=0) s -= U[i-1][j];
+			if(i+1< N-1) s -= U[i+1][j];
+			U[i][j] = s/(-4.0);
+		}
+	}
+	for(i=N-2;i>=0;i--) 
+	{
+		for(j=0;j<N-1;++j)
+		{
+			s = F[i][j];
+			if(j-1>=0) s -= U[i][j-1];
+			if(j+1< N-1) s -= U[i][j+1];
+			if(i-1>=0) s -= U[i-1][j];
+			if(i+1< N-1) s -= U[i+1][j];
+			U[i][j] = s/(-4.0);
+		}
+	}
+	for(i=N-2;i>=0;i--) 
+	{
+		for(j=N-2;j>=0;j--)
+		{
+			s = F[i][j];
+			if(j-1>=0) s -= U[i][j-1];
+			if(j+1< N-1) s -= U[i][j+1];
+			if(i-1>=0) s -= U[i-1][j];
+			if(i+1< N-1) s -= U[i+1][j];
+			U[i][j] = s/(-4.0);
+		}
+	}
+	*/
+	return 0;
+}
+int Multigrid_Iteration(double **U, double **F, int N)
+{
+	int i, j;
+	double s, r, **R, **Un, **Fn;
+	if(N == 2)				// N==2，未知數 => 1個 
+	{
+		U[0][0] = -F[0][0]/4.0;
+	}
+	else
+	{
+		// open memory for next level
+		R = (double **) malloc( (N-1)*sizeof(double*) );
+		R[0] = (double *) malloc( (N-1)*(N-1)*sizeof(double) );
+		for(i=1;i<N-1;++i) R[i] = R[i-1] + (N-1);		
+		Un = (double **) malloc( (N/2-1)*sizeof(double*) );					// U => E
+		Un[0] = (double *) malloc( (N/2-1)*(N/2-1)*sizeof(double) );
+		for(i=1;i<N/2-1;++i) Un[i] = Un[i-1] + (N/2-1);	
+		Fn = (double **) malloc( (N/2-1)*sizeof(double*) );					// F => R
+		Fn[0] = (double *) malloc( (N/2-1)*(N/2-1)*sizeof(double) );
+		for(i=1;i<N/2-1;++i) Fn[i] = Fn[i-1] + (N/2-1);
+
+		//Jacobi_Iteration(U,F,N);
+		GaussSeidel_Iteration(U,F,N);
+		r = Residual(R,U,F,N);
+		for(i=0;i<N/2-1;++i)
+		{
+			for(j=0;j<N/2-1;++j)			// 投影 
+			{
+				Un[i][j] = 0.0;
+				Fn[i][j] = 4*R[2*i+1][2*j+1];								// 幾何方式 
+				/*
+				Fn[i][j] = ((R[2*i][2*j]+2*R[2*i][2*j+1]+R[2*i][2*j+2])+
+				           (2*R[2*i+1][2*j]+4*R[2*i+1][2*j+1]+2*R[2*i+1][2*j+2])+
+						   (R[2*i+2][2*j]+2*R[2*i+2][2*j+1]+R[2*i+2][2*j+2]))/4.0;	//代數方式 ， 可試試看哪個好。 
+				*/
+			}
+		}
+		Multigrid_Iteration(Un,Fn,N/2);
+		for(i=0;i<N-1;++i)
+		{
+			for(j=0;j<N-1;++j)
+			{
+				if(i%2==1 && j%2 == 1) 
+				{
+					R[i][j] = Un[(i-1)/2][(j-1)/2];
+				}
+				
+				if(i%2==1 && j%2 == 0) 
+				{
+					R[i][j] = 0.0;
+					if(j/2-1>=0) R[i][j] += Un[(i-1)/2][j/2-1];
+					if(j/2<N/2-1) R[i][j] += Un[(i-1)/2][j/2];
+					R[i][j] *= 0.5;
+				}
+				if(i%2==0 && j%2 == 1) 
+				{
+					R[i][j] = 0.0;
+					if(i/2-1>=0) R[i][j] += Un[i/2-1][(j-1)/2];
+					if(i/2<N/2-1) R[i][j] += Un[i/2][(j-1)/2];
+					R[i][j] *= 0.5;
+				}
+				if(i%2==0 && j%2 == 0) 
+				{
+					R[i][j] = 0.0;
+					if(j/2-1>=0&&i/2-1>=0) R[i][j] += Un[i/2-1][j/2-1];
+					if(j/2<N/2-1&&i/2-1>=0) R[i][j] += Un[i/2-1][j/2];
+					if(j/2-1>=0&&i/2<N/2-1) R[i][j] += Un[i/2][j/2-1];
+					if(j/2<N/2-1&&i/2<N/2-1) R[i][j] += Un[i/2][j/2];
+					R[i][j] *= 0.25;
+				}
+			}
+		}
+		for(i=0;i<N-1;++i)
+		{
+			for(j=0;j<N-1;++j)
+			{
+				U[i][j] += R[i][j];
+			}
+		}
+		//GaussSeidel_Iteration(U,F,N);
+		free(R[0]);
+		free(R);
+		free(Un[0]);
+		free(Un);
+		free(Fn[0]);
+		free(Fn);
+	}
+	return 0;
+}
+double Residual(double **R, double **U, double **F, int N)
+{
+	int i, j;
+	double s, r=0.0;
+	for(i=0;i<N-1;++i) 
+	{
+		for(j=0;j<N-1;++j)
+		{
+			s = F[i][j]-U[i][j]*(-4);		// R[i][j]=F[i][j]-(-4)*U[i][j]-U[i][j-1]-U[i][j+1]-U[i-1][j]-U[i+1][j]
+			if(j-1>=0) s -= U[i][j-1];
+			if(j+1< N-1) s -= U[i][j+1];
+			if(i-1>=0) s -= U[i-1][j];
+			if(i+1< N-1) s -= U[i+1][j];
+			if(fabs(s) > r) r = fabs(s); 	// r = max_{i,j}|R[i][j]|
+			R[i][j] = s;
+		}
+	}
+	return r;
+}
